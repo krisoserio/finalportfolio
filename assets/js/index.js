0346a7e5d360a8ba55f3a3c375abbc4a02ca98cc
krisoserio <khristianoserio50@gmail.com>
2021-02-24 11:53:09 +0800

    update navabar in mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
     $('.toggle').click(function(){
         
             const toggleMenu = document.querySelector('.toggle');
-            toggleMenu.classList.toggle('active');
+            toggleMenu.classList.toggle('active')
+
+            const glassMorp = document.querySelector('.glass');
+            glassMorp.classList.toggle('show');
     });
 });
